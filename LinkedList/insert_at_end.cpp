e0d29035c900596a03993fa04d61e2f1e4f81dd0
Mayur Tamkhane <mayurtamkhane96@gmail.com>
2025-06-22 12:40:16 +0530

    Added insert at end program in linked list

---
+++ LinkedList/insert_at_end.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* next;
+};
+
+// Function to insert node at end
+void insertAtEnd(Node*& head, int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
+
+// Function to print the linked list
+void printList(Node* head) {
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Node* head = nullptr;
+
+    insertAtEnd(head, 10);
+    insertAtEnd(head, 20);
+    insertAtEnd(head, 30);
+    insertAtEnd(head, 40);
+
+    cout << "Linked List after inserting at end:\n";
+    printList(head);
+
+    return 0;
+}
--- LinkedList/push_program.bat
+++ LinkedList/push_program.bat
@@ -1,14 +1,20 @@
 @echo off
+:: Ask file name
 set /p filename=Enter program file name (without .cpp): 
-g++ %filename%.cpp -o %filename%
+
+:: Compile C++ code
+g++ "%filename%.cpp" -o "%filename%.exe"
+
+:: Run compiled program
 echo.
 echo Running Program Output:
 echo ------------------------
-%filename%
+"%filename%.exe"
 echo ------------------------
 
+:: Git commands
 git add .
-git commit -m "Added %filename%.cpp"
+git commit -m "Auto: Added %filename%.cpp"
 git push
 
 pause
